ef5fbf574fbb9f459bfd45cb9b8e459cac1d1c53
Viviana Garcia <vivianandrea4@gmail.com>
2020-10-29 17:40:02 -0400

    hamburger commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.navBarMobile');
+let bod = document.querySelector('.container')
+
+hamburger.addEventListener('click', function () {
+    // hamburger.classList.toggle('isactive');
+    menu.classList.toggle('active');
+});
+
